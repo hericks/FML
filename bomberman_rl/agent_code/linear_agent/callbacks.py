Fixed bug and added normalization of features.

bomberman_rl/agent_code/linear_agent/callbacks.py
@@ -10,8 +10,10 @@ import sys
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT']
 
+AGENT_NAME = "linear_agent"
+
 NUM_ACTIONS = len(ACTIONS)
-NUM_FEATURES = 2*9*9
+NUM_FEATURES = 2*31*31
 
 EPSILON_TRAIN = 0.3
 EPSILON_PLAY = 0.2
@@ -68,11 +70,51 @@ def act(self, game_state: dict) -> str:
         self.logger.debug("Choosing action purely at random.")
         return np.random.choice(ACTIONS, p=[.225, .225, .225, .225, .1])
         
+    action_map = normalize_state(game_state)
     features = state_to_features(game_state)
     q_values = np.dot(self.weights, features)
-    self.logger.debug(f"Choosing action {ACTIONS[np.argmax(q_values)]}.")
-    return ACTIONS[np.argmax(q_values)]
-
+    self.logger.debug(f"Choosing action {action_map(ACTIONS[np.argmax(q_values)])}.")
+    return action_map(ACTIONS[np.argmax(q_values)])
+    
+def normalize_state(game_state):
+    agent_x, agent_y = game_state['self'][3]
+    
+    def flip_tuple_x(t):
+        return (16 - t[0], t[1])
+        
+    def flip_tuple_y(t):
+        return (t[0], 16 - t[1])
+   
+    flipped_x = False
+    if agent_x > 8:
+        game_state['field'] = np.flipud(game_state['field'])
+        game_state['bombs'] = [(flip_tuple_x(pos), time) for pos, time in game_state['bombs']]
+        game_state['explosion_map'] = np.flipud(game_state['explosion_map'])
+        game_state['coins'] = [flip_tuple_x(coin) for coin in game_state['coins']]
+        name, score, canPlaceBomb, pos = game_state['self']
+        game_state['self'] = (name, score, canPlaceBomb, flip_tuple_x(pos))
+        game_state['others'] = [(name, score, canPlaceBomb, flip_tuple_x(pos)) for name, score, canPlaceBomb, pos in game_state['others']]
+        flipped_x = True
+        
+    flipped_y = False
+    if agent_y > 8:
+        game_state['field'] = np.fliplr(game_state['field'])
+        game_state['bombs'] = [(flip_tuple_y(pos), time) for pos, time in game_state['bombs']]
+        game_state['explosion_map'] = np.fliplr(game_state['explosion_map'])
+        game_state['coins'] = [flip_tuple_y(coin) for coin in game_state['coins']]
+        name, score, canPlaceBomb, pos = game_state['self']
+        game_state['self'] = (name, score, canPlaceBomb, flip_tuple_y(pos))
+        game_state['others'] = [(name, score, canPlaceBomb, flip_tuple_y(pos)) for name, score, canPlaceBomb, pos in game_state['others']]
+        flipped_y = True
+        
+    def action_map(a):
+        if flipped_x:
+            a = 'RIGHT' if a == 'LEFT' else ('LEFT' if a == 'RIGHT' else a)
+        if flipped_y:
+            a = 'UP' if a == 'DOWN' else ('DOWN' if a == 'UP' else a)
+        return a
+        
+    return action_map
 
 def state_to_features(game_state: dict) -> np.array:
     """
@@ -89,23 +131,23 @@ def state_to_features(game_state: dict) -> np.array:
        
     self_x, self_y = game_state['self'][3]
    
-    wall_map = np.zeros((29, 29)) 
+    wall_map = np.zeros((31, 31)) 
     field = game_state['field'] 
-    for x in np.arange(1, 16):
-        for y in np.arange(1, 16):
+    for x in np.arange(17):
+        for y in np.arange(17):
             if (field[x, y] != -1):
                 continue 
             
             x_rel, y_rel = x - self_x, y - self_y
-            wall_map[14 + x_rel, 14 + y_rel] = 1
+            wall_map[15 + x_rel, 15 + y_rel] = 1
             
-    coin_map = np.zeros((29, 29)) 
+    coin_map = np.zeros((31, 31)) 
     coins = game_state['coins']
     for x, y in coins:
         x_rel, y_rel = x - self_x, y - self_y
-        coin_map[14 + x_rel, 14 + y_rel] = 1
+        coin_map[15 + x_rel, 15 + y_rel] = 1
         
-    channels = [wall_map[10:19,10:19], coin_map[10:19,10:19]]
-
+    channels = [wall_map, coin_map]
+    
     return np.stack(channels).reshape(-1)
 