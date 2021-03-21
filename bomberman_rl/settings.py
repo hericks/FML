9e56695163cba71b0aa9116ad683c840d02cdcc3
Matthias Hericks <matthias.hericks@gmail.com>
2021-03-21 22:13:14 +0100

    Newest settings.

--- bomberman_rl/settings.py
+++ bomberman_rl/settings.py
@@ -5,11 +5,11 @@ from fallbacks import pygame
 # Game properties
 COLS = 17
 ROWS = 17
-CRATE_DENSITY = 0.95
+CRATE_DENSITY = 0.1
 MAX_AGENTS = 4
 
 # Round properties
-MAX_STEPS = 3000
+MAX_STEPS = 400
 
 # GUI properties
 GRID_SIZE = 30
@@ -40,7 +40,7 @@ INPUT_MAP = {
 }
 
 # Custom settings
-COIN_MULTIPLIER = 3
+COIN_MULTIPLIER = 1
 
 # Logging levels
 LOG_GAME = logging.INFO
