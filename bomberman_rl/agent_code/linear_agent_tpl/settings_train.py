3a78ab100dd3f87db0ae3eaf18de6603a79424fb
Matthias Hericks <matthias.hericks@gmail.com>
2021-03-25 22:49:13 +0100

    Added hooks for external simulation study script.

--- bomberman_rl/agent_code/linear_agent_tpl/settings_train.py
+++ bomberman_rl/agent_code/linear_agent_tpl/settings_train.py
@@ -1,4 +1,11 @@
+import os
 import events as e
+
+# --- Simulation study
+EXTERNAL_CONTROL = True
+
+# --- History settings
+AGENT_NAME = "N-STEP-SARSA-N=1"
 
 # Hyper parameters
 LEARNING_RATE = 0.0001
@@ -13,8 +20,8 @@ EPSILON_TRAIN_BREAKS = [0, 250]
 
 # Settings for TRAIN_POLICY_TYPE == 'SOFTMAX'
 # lower -> more randomness; higher -> more greedy
-INVERSE_TEMPERATURE_TRAIN_VALUES = [75, 75, 1000]
-INVERSE_TEMPERATURE_TRAIN_BREAKS = [0, 100, 150]
+INVERSE_TEMPERATURE_TRAIN_VALUES = [75]
+INVERSE_TEMPERATURE_TRAIN_BREAKS = [0]
 
 # --- Learning settings
 UPDATE_ALGORITHM = 'N-STEP-SARSA'
@@ -23,7 +30,11 @@ UPDATE_ALGORITHM = 'N-STEP-SARSA'
 None
 
 # Settings for UPDATE_ALGORITHM == 'N-STEP-SARSA'
-NUM_SARSA_STEPS = 5
+NUM_SARSA_STEPS = 1
+
+# Settings for UPDATE_ALGORITHM == 'SARSA(LAMBDA)'
+TRACE_DECAY = 0.9
+
 
 # --- Reward settings
 CONSTANT_REWARD = -0.1
@@ -32,5 +43,39 @@ EVENT_REWARDS = {
     e.COIN_COLLECTED: 1
 }
 
-# --- History settings
-AGENT_NAME = "linear_agent_tpl"
+# FOR EXTERNAL CONTROL (DO NOT CHANGE) -----------------------------------------
+
+import simulation_study_keys as k
+
+def valid_in_environ(key):
+    return key in os.environ and os.environ[key] != 'None'
+
+
+def assign_settings_from_environ():
+    global AGENT_NAME
+    if valid_in_environ(k.AGENT_NAME_KEY):
+        AGENT_NAME = os.environ[k.AGENT_NAME_KEY] 
+    
+    global LEARNING_RATE
+    if valid_in_environ(k.LEARNING_RATE_KEY):
+        LEARNING_RATE = float(os.environ[k.LEARNING_RATE_KEY])
+    
+    global DISCOUNT_FACTOR
+    if valid_in_environ(k.DISCOUNT_FACTOR_KEY):
+        DISCOUNT_FACTOR = float(os.environ[k.DISCOUNT_FACTOR_KEY])
+    
+    global UPDATE_ALGORITHM
+    if valid_in_environ(k.UPDATE_ALGORITHM_KEY):
+        UPDATE_ALGORITHM = os.environ[k.UPDATE_ALGORITHM_KEY]
+        
+    global NUM_SARSA_STEPS
+    if valid_in_environ(k.NUM_SARSA_STEPS_KEY):
+        NUM_SARSA_STEPS = int(os.environ[k.NUM_SARSA_STEPS_KEY])
+    
+    global TRACE_DECAY
+    if valid_in_environ(k.TRACE_DECAY_KEY):
+        TRACE_DECAY = float(os.environ[k.TRACE_DECAY_KEY])
+
+
+if EXTERNAL_CONTROL:
+    assign_settings_from_environ()
