c66b43a857e66d3df0f81635d3ccb0e9f9b54741
Matthias Hericks <matthias.hericks@gmail.com>
2021-03-26 18:18:13 +0100

    Fixed offset-bug in get_positions_reached_by_bomb.

--- bomberman_rl/agent_code/linear_agent_tpl/custom_event_utils.py
+++ bomberman_rl/agent_code/linear_agent_tpl/custom_event_utils.py
@@ -1,6 +1,7 @@
 # --- Definitions of custom events
 CRATE_DESTROYING_BOMB_DROPPED = 'CRATE_DESTROYING_BOMB_DROPPED'
 
+
 # --- Helper functions to compute these events
 def is_pos_on_field(pos):
     return 0 <= pos[0] <= 16 and 0 <= pos[1] <= 16
@@ -9,7 +10,7 @@ def is_pos_on_field(pos):
 def get_positions_reached_by_bomb(field, bomb_pos):
     reached_positions = [bomb_pos]
 
-    offset_ranges = [range(4), range(-1, -1, -1)]
+    offset_ranges = [range(1, 4), range(-1, -4, -1)]
     for offset_range in offset_ranges:
         for x_offset in offset_range:
             new_pos = (bomb_pos[0] + x_offset, bomb_pos[1])
@@ -18,7 +19,7 @@ def get_positions_reached_by_bomb(field, bomb_pos):
             else:
                 reached_positions.append(new_pos)
         for y_offset in offset_range:
-            new_pos = (bomb_pos[0] + y_offset, bomb_pos[1])
+            new_pos = (bomb_pos[0], bomb_pos[1] + y_offset)
             if not is_pos_on_field(new_pos) or field[new_pos] == -1:
                 break
             else:
@@ -28,7 +29,6 @@ def get_positions_reached_by_bomb(field, bomb_pos):
 
 
 def num_crates_bomb_will_destroy(field, bomb_pos):
-    ret = 0
     positions_reached_by_bomb = get_positions_reached_by_bomb(field, bomb_pos)
     return len([pos for pos in positions_reached_by_bomb if field[pos] == 1])
 
